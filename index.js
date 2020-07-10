e52bb46f0121e1327421bac71bd90d04719ab437
iRuxu <rx6@qq.com>
2020-07-10 14:40:20 +0800

    图标路径

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ class JX3_QIXUE{
 
         //初始化字段
             this._qixue_url = __ossMirror + 'data/qixue/'
-            this._img_path = __iconPath + 'icon/'
+            this._img_path = __ossMirror + 'icon/'
             this._total_levels = 12     //奇穴共12重
             this._item_attr_list = [
                 'icon',    //图标id
